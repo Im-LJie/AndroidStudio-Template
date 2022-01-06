7ffa9570c772161af137370decebb8e8736d06c7
GitHub Action <action@github.com>
2022-01-06 13:04:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/imljie/androidstudiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.imljie.androidstudiotemplate.services
+
+import com.github.imljie.androidstudiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
